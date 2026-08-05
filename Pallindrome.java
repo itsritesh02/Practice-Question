020dbdfba5aff5b41ec2252150fc51ed5f3df819
itsritesh02 <itsritesh02@gmail.com>
2026-08-05 17:32:10 +0530

    first commit

---
+++ Pallindrome.java
@@ -0,0 +1,20 @@
+class Pallindrome{
+  static void loop(int n){
+    for(int i=1; i<=n;i++){
+
+      for(int j=i;j>=1;j--){
+      System.out.print(j);
+      }
+for(int j=2;j<=i;j++){
+  System.out.print(j);
+}
+
+
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    int n=8;
+    loop(n);
+  }
+}
